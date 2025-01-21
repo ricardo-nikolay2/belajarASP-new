49d6535a3582d12b5b7175f8b0fcfe915563cf60
Ricardo Nikolay <Ricardo@istw.co.id>
2025-01-21 07:27:10 +0700

    menambahkan file HaloDuniacontroller

---
+++ Controllers/HaloDuniaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace belajarASP_new.Controllers
+{
+    public class HaloDuniaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- obj/Debug/net8.0/belajarASP-new.AssemblyInfo.cs
+++ obj/Debug/net8.0/belajarASP-new.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("belajarASP-new")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11866986ef5108442b8488bf253a2d1101934507")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd98af6264a79ad22ad1991772054f808c4abcda")]
 [assembly: System.Reflection.AssemblyProductAttribute("belajarASP-new")]
 [assembly: System.Reflection.AssemblyTitleAttribute("belajarASP-new")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
